6ac959589eb20f56e5ada18cd159a1a4d0d12945
Matthew Garcia <gmatthew2765@gmail.com>
2026-02-17 17:04:00 -0800

    updated theme to be better and on-brand

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -79,11 +79,49 @@ return {
   -- },
   {
     "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- require("koda").setup({ transparent = true })
+      local koda = require("koda")
+
+      koda.setup({
+        colors = {
+          const = "#7788aa",
+        },
+        on_highlights = function(hl, c)
+          hl.Directory = { bold = true }
+          hl["Directory"] = {
+            fg = "#DEEEED",
+            bold = true,
+          }
+          hl["@function"] = { fg = "#7788aa", italic = true, bold = true }
+          hl["@keyword"] = { fg = "#7788aa", bold = true }
+          hl["@keyword.return"] = { fg = "#7788aa", bold = true }
+
+          hl["@tag"] = { bold = true, fg = "#7788aa" }
+          hl["@tag.builtin"] = { fg = "#DEEEED" }
+          hl["@tag.attribute"] = { fg = "#708090" }
+        end,
+      })
+
       vim.cmd("colorscheme koda")
+
+      local palette = koda.get_palette()
+      local my_primary = "#7788aa"
+
+      local snacks_fixes = {
+        SnacksPickerGitStatusIgnored = { fg = palette.fg },
+        SnacksPickerGitStatusUntracked = { fg = my_primary },
+        SnacksPickerPathIgnored = { fg = palette.comment },
+        SnacksPickerPathHidden = { fg = palette.fg },
+
+        SnacksPickerIconGitUntracked = { fg = my_primary },
+        SnacksPickerDir = { fg = my_primary },
+      }
+
+      for group, opts in pairs(snacks_fixes) do
+        vim.api.nvim_set_hl(0, group, opts)
+      end
     end,
   },
 }
